5882f5bb9d4ec43b93a15bb93d200ce42afc58cf
NU-LL <1125934312@qq.com>
2023-08-28 01:15:15 +0800

    NEW: init tiny200 proj

---
+++ board/allwinner/f1c200/post-image.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+main() {
+	local SCRIPT_PATH=$(dirname "$0")
+
+    cp ${SCRIPT_PATH}/splash.bmp ${BINARIES_DIR}
+
+    support/scripts/genimage.sh -c ${SCRIPT_PATH}/genimage-sdcard.cfg
+
+}
+
+main $@
